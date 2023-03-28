e9f8f936be54f249a12cf33e7611c6944891b409
Jin-Jin Lee <a11913405@unet.univie.ac.at>
2023-03-28 14:16:06 +0200

    removed unneccessary files and organized them

---
+++ source/sql/11913405_LEE_drop.sql
@@ -0,0 +1,15 @@
+DROP TABLE contains;
+DROP TABLE compilation;
+DROP TABLE comments;
+DROP TABLE needs;
+DROP TABLE ingredient;
+DROP TABLE beverage;
+DROP TABLE dish;
+DROP TABLE recipe;
+DROP TABLE friend;
+DROP TABLE users;
+DROP SEQUENCE users_sequence;
+
+DROP VIEW drinks;
+DROP VIEW wrote_at_least_2recipes;
+
